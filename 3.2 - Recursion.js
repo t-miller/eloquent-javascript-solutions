36718f4a5d5fd34b9f5d114cf8dca6368a9ea400
Tyler Miller <tyler.rw.miller@gmail.com>
2016-08-24 15:41:15 -0400

    fixed to handle negative numbers

--- 3.2 - Recursion.js
+++ 3.2 - Recursion.js
@@ -16,6 +16,9 @@ Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to f
 
 // Your code here.
 function isEven(number) {
+  if (number < 0) {
+    number *= -1
+  }
   if (number == 0) {
    return true;
   }
