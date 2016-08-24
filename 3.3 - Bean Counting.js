32857454a8a077d6f1b2259d2ef5b28013c4b9c0
Tyler Miller <tyler.rw.miller@gmail.com>
2016-08-24 16:48:13 -0400

    updated to add countChar and updated countBs

--- 3.3 - Bean Counting.js
+++ 3.3 - Bean Counting.js
@@ -7,14 +7,18 @@ Next, write a function called countChar that behaves like countBs, except it tak
 */
 
 // Your code here.
-function countBs(str) {
-  var numberOfBs = 0;
+function countBs(str) {  
+  return countChar(str, "B");
+};
 
+function countChar(str, char) {
+  var numberOfChars = 0;
+  
   for (var i = 0; i < str.length; i++) {
-    if (str.charAt(i) === "B") {
-      numberOfBs++;
+    if (str.charAt(i) === char) {
+      numberOfChars++;
     }
   }
   
-  return numberOfBs;
-};  
+  return numberOfChars;
+};
