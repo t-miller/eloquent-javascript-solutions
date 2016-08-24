d59d94aa94dc44ee8cf788f3757231f9ac3c3b98
Tyler Miller <tyler.rw.miller@gmail.com>
2016-08-24 11:38:54 -0400

    added problem statement

--- 2.1 - Looping a Triangle.js
+++ 2.1 - Looping a Triangle.js
@@ -1,3 +1,16 @@
+/*
+Looping a Triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
 // Your code here.
 var str = ""
 
