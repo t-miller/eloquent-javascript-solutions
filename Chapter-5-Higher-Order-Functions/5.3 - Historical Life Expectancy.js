6fb47108fb8aabff12f320c4f48d7764b76e8356
Tyler Miller <tyler.rw.miller@gmail.com>
2016-09-12 17:26:43 -0400

    initial commit (just the problem setup)

---
+++ Chapter-5-Higher-Order-Functions/5.3 - Historical Life Expectancy.js
@@ -0,0 +1,5 @@
+/*
+Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+
+For bonus points, write a function groupBy that abstracts the grouping operation. It should accept as arguments an array and a function that computes the group for an element in the array and returns an object that maps group names to arrays of group members.
+*/
