aa055bf054db436121e025e81ebed419c38bfec5
Tyler Miller <tyler.rw.miller@gmail.com>
2016-09-13 10:58:57 -0400

    initial commit - sorting in place

--- Chapter-5-Higher-Order-Functions/5.3 - Historical Life Expectancy.js
+++ Chapter-5-Higher-Order-Functions/5.3 - Historical Life Expectancy.js
@@ -3,3 +3,27 @@ Compute and output the average age of the people in the ancestry data set per ce
 
 For bonus points, write a function groupBy that abstracts the grouping operation. It should accept as arguments an array and a function that computes the group for an element in the array and returns an object that maps group names to arrays of group members.
 */
+// Your code here.
+function century(person) {
+  return Math.ceil(person.died / 100);
+};
+
+function groupBy(arr, func) {
+  var arrStore = {};  
+  var funcReturn;
+  
+  for (var i in arr) {
+    funcReturn = func(arr[i]);
+    console.log("fR: " + funcReturn);
+    if (arrStore.hasOwnProperty(funcReturn)) {
+      arrStore[funcReturn].push(arr[i]);      
+    }
+    else {
+      console.log("i: " + arr[i]);
+      arrStore[funcReturn] = [arr[i]];
+    }
+  }
+  return arrStore;  
+};
+
+console.log(groupBy(ancestry, century));
