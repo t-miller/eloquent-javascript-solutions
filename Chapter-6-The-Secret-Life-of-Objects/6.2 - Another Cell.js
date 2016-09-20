01c05757b8f076d0681b358cbef1c7ed66dd6f13
Tyler Miller <tyler.rw.miller@gmail.com>
2016-09-20 10:26:54 -0400

    initial problem setup

---
+++ Chapter-6-The-Secret-Life-of-Objects/6.2 - Another Cell.js
@@ -0,0 +1,5 @@
+/*
+Implement a cell type named StretchCell(inner, width, height) that conforms to the table cell interface described earlier in the chapter. It should wrap another cell (like UnderlinedCell does) and ensure that the resulting cell has at least the given width and height, even if the inner cell would naturally be smaller.
+*/
+
+// Your code here.
