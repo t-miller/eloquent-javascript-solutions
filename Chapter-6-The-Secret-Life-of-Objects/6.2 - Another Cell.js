0f60951a26016067367242f78689b7ca778779f7
Tyler Miller <tyler.rw.miller@gmail.com>
2016-09-20 13:36:46 -0400

    completed functions

--- Chapter-6-The-Secret-Life-of-Objects/6.2 - Another Cell.js
+++ Chapter-6-The-Secret-Life-of-Objects/6.2 - Another Cell.js
@@ -3,3 +3,21 @@ Implement a cell type named StretchCell(inner, width, height) that conforms to t
 */
 
 // Your code here.
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+};
+
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.inner.minWidth(), this.width);
+};
+
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.inner.minHeight() + 1, this.height);
+};
+
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([repeat(" ", width)]);
+};
