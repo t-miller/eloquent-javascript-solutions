4b1c7cef757409c9fc0ea5a5e4063833dd8c1f86
Tyler Miller <tyler.rw.miller@gmail.com>
2017-11-29 11:16:14 -0500

    fixed typo

--- Chapter-2-Program-Structure/2.1 - Looping a Triangle.js
+++ Chapter-2-Program-Structure/2.1 - Looping a Triangle.js
@@ -14,7 +14,7 @@ Write a loop that makes seven calls to console.log to output the following trian
 // Your code here.
 var str = ""
 
-for (var i = 1; i 7<= 7; i++){
+for (var i = 1; i <= 7; i++){
   str += "#";
   console.log(str);
 };
