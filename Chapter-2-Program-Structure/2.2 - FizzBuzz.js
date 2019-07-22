d7bfe04992d003c87424452142a02329c8fce3e1
Tyler Miller <tyler.rw.miller@gmail.com>
2019-07-22 09:00:35 -0400

    'var' -> 'let', reduce repetition

--- Chapter-2-Program-Structure/2.2 - FizzBuzz.js
+++ Chapter-2-Program-Structure/2.2 - FizzBuzz.js
@@ -1,23 +1,28 @@
 /*
 
-Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
-
-When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, 
+and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz", 
+for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 */
 
 // Your code here.
-for (var i = 1; i<=100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+for (let i = 1; i<=100; i++) {
+  let divBy3 = i % 3 == 0
+  let divBy5 = i % 5 == 0
+      
+  if (divBy3 && divBy5) {
+    console.log("FizzBuzz")
   }  
-  else if (i % 3 == 0 ) {
-    console.log("Fizz");
+  else if (divBy3) {
+    console.log("Fizz")
   }
-  else if (i % 5 == 0) {
-    console.log("Buzz");
+  else if (divBy5) {
+    console.log("Buzz")
   }
   else {
-    console.log(i);
+    console.log(i)
   }
 };
