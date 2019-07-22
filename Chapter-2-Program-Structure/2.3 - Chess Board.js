9395a8c09e247c4d7e26b58241db7b3886a5842c
Tyler Miller <tyler.rw.miller@gmail.com>
2019-07-22 09:31:13 -0400

    Redo to get working

--- Chapter-2-Program-Structure/2.3 - Chess Board.js
+++ Chapter-2-Program-Structure/2.3 - Chess Board.js
@@ -1,6 +1,7 @@
 /*
 
-Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
+At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 
 Passing this string to console.log should show something like this:
 
@@ -13,32 +14,32 @@ Passing this string to console.log should show something like this:
  # # # #
 # # # #
 
-When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, 
+outputting a grid of the given width and height.
 
 */
 
 // Your code here.
-var size = 8;
-var oddString = " # # # #";
-var evenString = "# # # # ";
-var finalString = "";
+let chessboard = (x) => {
+  let board = ""
+  for(let i = 0; i < x; i++) {
+    let evenRow = i % 2 == 0
 
-for (var i = 1; i <= size; i++) {
-  
-  if (i==size) {
-    if (i % 2 != 0) {
-    	finalString+=oddString;
-    }
-    else {
-    	finalString+=evenString;
-    }
-  }
-  else if (i % 2 != 0) {
-    finalString+=oddString + "\n";
-  }
-  else {
-    finalString+=evenString +"\n";
-  }
-};
+    for(let j = 0; j < x; j++) {
+      let evenCol = j % 2 == 0
 
-console.log(finalString);
+      if ((evenRow && evenCol) || (!evenRow && !evenCol)) {
+        board += "#"
+      }
+      else {
+        board += " "
+      }
+      if (j == x - 1 && i != x-1) {
+        board += "\n"
+      }
+    }
+  }
+  console.log(board)
+}
+
+chessboard(8)
