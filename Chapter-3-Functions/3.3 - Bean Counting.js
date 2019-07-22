829f35294491bde4aede68a6d658acc38ae86496
Tyler Miller <tyler.rw.miller@gmail.com>
2019-07-22 09:51:49 -0400

    'var' -> 'let'

--- Chapter-3-Functions/3.3 - Bean Counting.js
+++ Chapter-3-Functions/3.3 - Bean Counting.js
@@ -12,9 +12,9 @@ function countBs(str) {
 };
 
 function countChar(str, char) {
-  var numberOfChars = 0;
+  let numberOfChars = 0;
   
-  for (var i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     if (str.charAt(i) === char) {
       numberOfChars++;
     }
