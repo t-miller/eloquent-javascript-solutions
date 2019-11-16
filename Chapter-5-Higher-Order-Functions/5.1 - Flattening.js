202c9226eb59e3ed5acfcf7884d3d2b64f5c8032
Tyler Miller <tyler.rw.miller@gmail.com>
2019-11-16 13:39:28 -0500

    Reformat

--- Chapter-5-Higher-Order-Functions/5.1 - Flattening.js
+++ Chapter-5-Higher-Order-Functions/5.1 - Flattening.js
@@ -1,7 +1,14 @@
 /*
-Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the input arrays.
+Use the reduce method in combination with the concat method to “flatten” an
+array of arrays into a single array that has all the elements of the input
+arrays.
 */
+const arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-console.log(arrays.reduce(function(a,b) {  
-  return a.concat(b);
-}));
+console.log(
+    arrays.reduce(
+        (a, b) => {
+          return a.concat(b);
+        }
+    )
+);
